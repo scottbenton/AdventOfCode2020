b664244d8d2a27f8f2901839926cf1197798c76f
Scott Benton <scott@scottbenton.dev>
2020-12-16 17:54:13 -0500

    Added a function to day 15 to print times of different options

--- Day15/RambunctiousRecitation.js
+++ Day15/RambunctiousRecitation.js
@@ -1,3 +1,5 @@
+const { performance } = require("perf_hooks");
+
 function findNthSpokenWordUsingObjects(starter, n) {
   let lastSeenIndexes = {};
   starter.slice(0, starter.length - 1).forEach((value, idx) => {
@@ -32,9 +34,26 @@ function findNthSpokenWordUsingMaps(starter, n) {
   return lastNumber;
 }
 
+function printExecutionTimes(input, n) {
+  console.log(`Times for ${n} items:`);
+
+  let objectT0 = performance.now();
+  findNthSpokenWordUsingObjects(input, n);
+  let objectT1 = performance.now();
+
+  console.log(`Object: ${objectT1 - objectT0} milliseconds`);
+
+  let mapT0 = performance.now();
+  findNthSpokenWordUsingMaps(input, n);
+  let mapT1 = performance.now();
+
+  console.log(`Map: ${mapT1 - mapT0} milliseconds`);
+}
+
 function main() {
   const input = [14, 3, 1, 0, 9, 5]; // No file today, just this :)
 
+  // Solution Using Objects
   // console.log("--Part 1--");
   // const number2020 = findNthSpokenWordUsingObjects(input, 2020);
   // console.log("2020th spoken number: " + number2020);
@@ -43,6 +62,7 @@ function main() {
   // const number30000000 = findNthSpokenWordUsingObjects(input, 30000000);
   // console.log("30000000th spoken word: " + number30000000);
 
+  // Solution using maps
   console.log("--Part 1--");
   const number2020 = findNthSpokenWordUsingMaps(input, 2020);
   console.log("2020th spoken number: " + number2020);
@@ -50,6 +70,9 @@ function main() {
   console.log("--Part 2--");
   const number30000000 = findNthSpokenWordUsingMaps(input, 30000000);
   console.log("30000000th spoken word: " + number30000000);
+
+  // Prints execution times for objects and for maps
+  // printExecutionTimes(input, 30000000);
 }
 
 main();
